3b39f8ceda0f23ad09b21f17c07061daa54d2cc8
Sankalp <sankalpdhama11@gmail.com>
2023-08-20 19:23:03 +0530

    final Push

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -9,19 +9,19 @@ class Responsive extends StatelessWidget {
   static bool isMobile(BuildContext context)=>
       MediaQuery.of(context).size.width<=500;
   static bool isMobileLarge(BuildContext context)=>
-      MediaQuery.of(context).size.width<=700;
+      MediaQuery.of(context).size.width<=800;
   static bool isTablet(BuildContext context)=>
-      MediaQuery.of(context).size.width<1024;
+      MediaQuery.of(context).size.width<1060;
   static bool isDesktop(BuildContext context)=>
-      MediaQuery.of(context).size.width>=1024;
+      MediaQuery.of(context).size.width>=1060;
   @override
   Widget build(BuildContext context) {
     final Size _size=MediaQuery.of(context).size;
-    if(_size.width>=1024){
+    if(_size.width>=1060){
       return desktop!;
-    }else if(_size.width>=700 && tablet!=null){
+    }else if(_size.width>=800 && tablet!=null){
       return tablet;
-    }else if(_size.width>=500 && mobileLarge!=null){
+    }else if(_size.width>=600 && mobileLarge!=null){
       return mobileLarge!;
     }else{
       return mobile;
